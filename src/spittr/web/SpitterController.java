3fddb882dc7e9e8dfe33eff8e9866048d597d23e
ZhongHao <zh826256645@gmail.com>
2016-10-05 17:43:03 +0800

    使用 flash 属性保存重定向前的数据
    实际上是使用 Session 保存，
    不过由 Spring 管理这个属性的生命周期

--- src/spittr/web/SpitterController.java
+++ src/spittr/web/SpitterController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import spittr.Spitter;
 import spittr.data.SpittlerRepository;
@@ -19,7 +20,7 @@ public class SpitterController {
 
 
 	private SpittlerRepository spittlerRepository;
-	
+
 	@Autowired
 	public SpitterController(SpittlerRepository spittlerRepository) {
 		this.spittlerRepository = spittlerRepository;
@@ -28,7 +29,7 @@ public class SpitterController {
 	public SpitterController() {
 		super();
 	}
-	
+
 	// 访问注册页面
 	// URI:/spitter/register GET
 	// return:registerForm.jsp
@@ -37,7 +38,7 @@ public class SpitterController {
 		model.addAttribute(new Spitter());
 		return "registerForm";
 	}
-	
+
 	// 用户注册
 	// URI:/spitter/register POST
 	@RequestMapping(value="/register", method=RequestMethod.POST)
@@ -45,17 +46,21 @@ public class SpitterController {
 			// 启动校验
 			@Valid Spitter spitter,
 			// 保存校验出现的错误
-			Errors errors) {
+			Errors errors, 
+			RedirectAttributes model) {
 		// 校验出错，跳转到登录页面
 		if (errors.hasErrors()) {
 			return "registerForm";
 		}
 		// 保存用户
-		spittlerRepository.save(spitter);
-		// 重定向到用户页面
-		return "redirect:/spitter/" + spitter.getUsername();
+		Spitter spitter_this = spittlerRepository.save(spitter);
+		// 将 Spitter 数据保存到 flash 属性中，重定向后取出
+		model.addFlashAttribute("spitter", spitter_this);
+		model.addAttribute("username", spitter_this.getUsername());
+		// username 以模板的形式填充到 URL 中，这样更安全
+		return "redirect:/spitter/{username}";
 	}
-	
+
 	// 用户页面
 	// URI：/spitter/{username} GET
 	@RequestMapping(value="/{username}", method=RequestMethod.GET)
@@ -63,8 +68,11 @@ public class SpitterController {
 			@PathVariable("username") final String username,
 			Model model
 			) {
-		model.addAttribute(spittlerRepository.
-				findByUsername(username));
+		// 如果模型中不存在 spitter 属性，去数据库中查询，存在直接转发到 view
+		if(!model.containsAttribute("spitter")) {
+			model.addAttribute(spittlerRepository.
+					findByUsername(username));
+		}
 		return "profile";
 	}
 }
